a8a195215a7e5f8e395ab677dd7895cbd9dfaec7
SHIVAMPURA <100090814+SHIVAMPURA@users.noreply.github.com>
2025-03-24 23:39:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
-Test
+resource "google_compute_instance" "vm-demo-spacelift" {
+  name         = "vm-demo-spacelift"
+  machine_type = "e2-micro"  
+  zone         = "us-central1-a"  
+  boot_disk {
+    initialize_params {
+      image = "debian-11-bullseye-v20220216"  
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+}
+
